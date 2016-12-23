fc67e25c8486dd18ba3bb2deb622de92f77471b3
Joao Ribeiro <joao.ribeiro@williamhill.com>
2016-12-23 23:54:15 +0000

    Lesson 1

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,17 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
-(defn square [x]
-  ":(")
+(defn
+  square
+  "Takes a number as a parameter and multiplies it with itself."
+  [x]
+  (* x x))
 
-(defn average [a b]
-  ":(")
+(defn
+  average
+  "Takes two numbers and return their average"
+  [a b]
+  (/ (+ a b) 2))
